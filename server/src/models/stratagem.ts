738cedbd476247b93b270b6eedda378e95921c57
Aless00san <mumaitronzx@gmail.com>
2025-09-01 20:20:32 +0200

    Refactored the stratagem model

--- server/src/models/stratagem.ts
+++ server/src/models/stratagem.ts
@@ -1,8 +1,14 @@
 import { Prisma } from '@prisma/client';
 
+type JsonValue = string | number | boolean | null | JsonObject | JsonArray;
+interface JsonObject {
+  [key: string]: JsonValue;
+}
+interface JsonArray extends Array<JsonValue> {}
+
 export type StratagemInput = {
   name: string;
-  code: Prisma.JsonValue;
+  code: JsonValue;
   category: string;
   enabled: boolean;
 };
